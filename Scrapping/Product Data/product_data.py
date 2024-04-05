b5b66b108b2ab5bcec09de0f5bd3b42121c9dd85
Subhrajit Samanta <100481343+SrtSaa@users.noreply.github.com>
2024-04-05 22:21:02 +0530

    modified for to collect data

--- Scrapping/Product Data/product_data.py
+++ Scrapping/Product Data/product_data.py
@@ -9,11 +9,15 @@ import pandas as pd
 
 ##### Required variables #####
 
-# apis to collect data for eyeglasses, computer glasses, and sunglasses
-eyeglass_api = "https://api-gateway.juno.lenskart.com/v2/products/category/3363?page-size=25&page="
-computer_Glass_api = "https://api-gateway.juno.lenskart.com/v2/products/category/8427?page-size=25&page="
-sunglass_api = "https://api-gateway.juno.lenskart.com/v2/products/category/3362?page-size=25&page=0"
+# API format
+# "https://api-gateway.juno.lenskart.com/v2/products/category/[ID for glass]?page-size=25&page="
 
+# id for type of glasses correspondingly
+glassType = {
+    # "eyeglass": "3363",
+    "computer_glass": "8427"
+    # "sunglass": "3362"
+}
 
 # attributes of dataframe
 cols = [
@@ -45,127 +49,52 @@ sunglass_df = pd.DataFrame(columns = cols)
 
 
 
-##### Eyeglasses data #####
+##### Collecting Data #####
 
-# # current page
-# curr_page = 0
+for Gtype in glassType:
+    # current page
+    curr_page = 0
 
-# # get data from api
-# res = requests.get(eyeglass_api + str(curr_page))
+    # empty dataframe
+    df = pd.DataFrame(columns = cols)
 
-# # extract data from api
-# print("Start collecting data for eyeglasses...")
-# while res.status_code == 200:
-#     data = json.loads(res.text)
-#     products = data['result']['product_list']
-#     if len(products) == 0:
-#         break
-#     for product in products:
-#         details = []
-#         for item in cols:
-#             try:
-#                 if item == "market_price":
-#                     value = product['prices'][0]['price']
-#                 elif item == "lenskart_price":
-#                     value = product['prices'][1]['price']
-#                 else:
-#                     value = product[item]
-#             except:
-#                 value = None
-#             details.append(value)
-#         eyeglass_df.loc[len(eyeglass_df.index)] = details
-#     curr_page += 1
-#     res = requests.get(eyeglass_api + str(curr_page))
+    # API
+    api = "https://api-gateway.juno.lenskart.com/v2/products/category/" + glassType[Gtype] + "?page-size=25&page=" 
 
+    # get data from api
+    res = requests.get(api + str(curr_page))
 
-# # show data
-# print(eyeglass_df)
+    # extract data from api
+    print("Start collecting data for",Gtype,"...")
+    while res.status_code == 200:
+        print("Page:", curr_page, end='\r')
+        data = json.loads(res.text)
+        products = data['result']['product_list']
+        if len(products) == 0:
+            break
+        for product in products:
+            details = []
+            for item in cols:
+                try:
+                    if item == "market_price":
+                        value = product['prices'][0]['price']
+                    elif item == "lenskart_price":
+                        value = product['prices'][1]['price']
+                    else:
+                        value = product[item]  
+                except:
+                    value = None
+                details.append(value)
+            df.loc[len(df.index)] = details
+        curr_page += 1
+        res = requests.get(api + str(curr_page))
 
-# # convert the data into csv
-# eyeglass_df.to_csv('eyeglass_data.csv')
-# print("Data collection is completed for Eyeglasses...")
+    # show data
+    print(df)
 
+    # file name
+    filename = Gtype + "_data.csv"
 
-
-
-
-##### Computer Glasses data #####
-
-# # current page
-# curr_page = 0
-
-# # get data from api
-# res = requests.get(computer_Glass_api + str(curr_page))
-
-# # extract data from api
-# print("Start collecting data for Computer Glasses...")
-# while res.status_code == 200:
-#     data = json.loads(res.text)
-#     products = data['result']['product_list']
-#     if len(products) == 0:
-#         break
-#     for product in products:
-#         details = []
-#         for item in cols:
-#             try:
-#                 if item == "market_price":
-#                     value = product['prices'][0]['price']
-#                 elif item == "lenskart_price":
-#                     value = product['prices'][1]['price']
-#                 else:
-#                     value = product[item]
-#             except:
-#                 value = None
-#             details.append(value)
-#         computer_Glass_df.loc[len(computer_Glass_df.index)] = details
-#     curr_page += 1
-#     res = requests.get(computer_Glass_api + str(curr_page))
-
-# # show data
-# print(computer_Glass_df)
-
-# # convert the data into csv
-# computer_Glass_df.to_csv('computer_Glass_data.csv')
-# print("Data collection is completed for Computer Glasses...")
-
-
-
-
-
-##### Sunglasses data #####
-# current page
-curr_page = 0
-
-# get data from api
-res = requests.get(sunglass_api + str(curr_page))
-
-# extract data from api
-print("Start collecting data for Sunglasses...")
-while res.status_code == 200:
-    data = json.loads(res.text)
-    products = data['result']['product_list']
-    if len(products) == 0:
-        break
-    for product in products:
-        details = []
-        for item in cols:
-            try:
-                if item == "market_price":
-                    value = product['prices'][0]['price']
-                elif item == "lenskart_price":
-                    value = product['prices'][1]['price']
-                else:
-                    value = product[item]  
-            except:
-                value = None
-            details.append(value)
-        sunglass_df.loc[len(sunglass_df.index)] = details
-    curr_page += 1
-    res = requests.get(sunglass_api + str(curr_page))
-
-# show data
-print(sunglass_df)
-
-# convert the data into csv
-sunglass_df.to_csv('sunglass_data.csv')
-print("Data collection is completed for Sunglasses...")
+    # convert the data into csv
+    df.to_csv(filename, index=False)
+    print("Data collection is completed for for",Gtype,"...")
